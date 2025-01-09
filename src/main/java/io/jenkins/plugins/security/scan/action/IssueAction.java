6e476c9b46471035fa8931493beaac6285399164
Sadman Anik <sadmana@blackduck.com>
2025-01-09 17:20:09 +0600

    Fixed product name style

--- src/main/java/io/jenkins/plugins/security/scan/action/IssueAction.java
+++ src/main/java/io/jenkins/plugins/security/scan/action/IssueAction.java
@@ -2,11 +2,21 @@ package io.jenkins.plugins.security.scan.action;
 
 import hudson.model.Action;
 import io.jenkins.plugins.security.scan.global.ApplicationConstants;
+import java.util.HashMap;
+import java.util.Map;
 
 public class IssueAction implements Action {
     private final String product;
     private final int defectCount;
     private final String issueViewUrl;
+
+    private static final Map<String, String> PRODUCT_NAME_MAP;
+
+    static {
+        PRODUCT_NAME_MAP = new HashMap<>();
+        PRODUCT_NAME_MAP.put("polaris", "Polaris");
+        PRODUCT_NAME_MAP.put("srm", "SRM");
+    }
 
     public IssueAction(final String product, final int defectCount, final String issueViewUrl) {
         this.product = product;
@@ -21,11 +31,15 @@ public class IssueAction implements Action {
 
     @Override
     public String getDisplayName() {
-        return String.format("See issues in %s (%d found)", product, defectCount);
+        return String.format("See issues in %s (%d found)", getDisplayNameForProduct(product), defectCount);
     }
 
     @Override
     public String getUrlName() {
         return issueViewUrl;
+    }
+
+    private String getDisplayNameForProduct(String product) {
+        return PRODUCT_NAME_MAP.getOrDefault(product.toLowerCase(), product);
     }
 }
