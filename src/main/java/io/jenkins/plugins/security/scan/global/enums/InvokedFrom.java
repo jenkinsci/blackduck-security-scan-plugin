2a5acc2cdf33ef4dc9418a3edaa5c2879c9e3007
Akib Uz Zaman <akibuz@blackduck.com>
2025-06-20 15:59:59 +0600

    update values of InvokedFrom enum

--- src/main/java/io/jenkins/plugins/security/scan/global/enums/InvokedFrom.java
+++ src/main/java/io/jenkins/plugins/security/scan/global/enums/InvokedFrom.java
@@ -3,12 +3,12 @@ package io.jenkins.plugins.security.scan.global.enums;
 public enum InvokedFrom {
     INT_JENKINS_FREESTYLE("Integrations-jenkins-freestyle"),
     INT_JENKINS_PIPELINE("Integrations-jenkins-pipeline"),
-    INT_GITHUB_CLOUD("Integrations-github-cloud"),
-    INT_GITHUB_EE("Integrations-github-ee"),
-    INT_BITBUCKET_CLOUD("Integrations-bitbucket-cloud"),
-    INT_BITBUCKET_EE("Integrations-bitbucket-ee"),
-    INT_GITLAB_CLOUD("Integrations-gitlab-cloud"),
-    INT_GITLAB_EE("Integrations-gitlab-ee");
+    INT_GITHUB_CLOUD("Integrations-jenkins-github-cloud"),
+    INT_GITHUB_EE("Integrations-jenkins-github-ee"),
+    INT_BITBUCKET_CLOUD("Integrations-jenkins-bitbucket-cloud"),
+    INT_BITBUCKET_EE("Integrations-jenkins-bitbucket-ee"),
+    INT_GITLAB_CLOUD("Integrations-jenkins-gitlab-cloud"),
+    INT_GITLAB_EE("Integrations-jenkins-gitlab-ee");
 
     private final String value;
 
